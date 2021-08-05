b689ac767e412bd0249511415432b671a681fd9e
HoukyShen <786501105@qq.com>
2021-08-05 17:47:10 +0800

    画主页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,76 @@
 <template>
   <div>
-    <h2>Dashboard</h2>
-    <el-button @click.native.prevent="logout">注销</el-button>
-    <el-button @click.native.prevent="getUserInfo">获取用户信息</el-button>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <el-table
+              ref="multipleTable"
+              :data="tableData"
+              tooltip-effect="dark"
+              style="width: 100%"
+              @selection-change="handleSelectionChange">
+            <el-table-column
+                type="selection"
+                width="55">
+            </el-table-column>
+            <el-table-column
+                label="日期"
+                width="120">
+              <template slot-scope="scope">{{ scope.row.date }}</template>
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="姓名"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                prop="address"
+                label="地址"
+                show-overflow-tooltip>
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 
 </template>
@@ -14,17 +82,51 @@ import * as Config from "@/settings";
 export default {
   name: "Dashboard",
   methods: {
-    logout(){
+    logout() {
       //请求后端接口删除该用户的token，同时前端Cookies也要删除该用户的token
-      this.$request.delete('http://localhost:8000/auth/logout').then(res=>{
+      this.$request.delete('http://localhost:8000/auth/logout').then(res => {
         removeToken(Config.TokenKey)
         this.$router.replace('/')
       })
     },
-    getUserInfo(){
-      this.$request.get('http://localhost:8000/auth/info').then(res=>{
+    getUserInfo() {
+      this.$request.get('http://localhost:8000/auth/info').then(res => {
         console.log(res.data);
-      },err=> err)
+      }, err => err)
+    }
+  },
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-08',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-07',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+      multipleSelection: []
     }
   }
 }

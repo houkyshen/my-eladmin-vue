9cf7e631e53e4c125285cf932f8aaf37464784e0
HoukyShen <786501105@qq.com>
2021-08-07 18:05:19 +0800

    获取菜单

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,14 @@
 <template>
   <div style="height: 100%">
     <el-container style="height: 100%">
-      <el-aside width="200px">
-        <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
+      <el-aside width="205px" >
+        <el-menu text-color="#bfcbd9" class="sidebar-container">
+          <el-submenu :index="item.name" v-for="item in menuList">
+            <template slot="title">{{item.meta.title}}</template>
+            <el-menu-item :index="child.name" v-for="child in item.children" style="padding-left: 5px">
+              <template slot="title">{{child.meta.title}}</template>
+            </el-menu-item>
           </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
         </el-menu>
       </el-aside>
       <el-container>
@@ -87,7 +61,16 @@ import * as Config from "@/settings";
 
 export default {
   name: "Dashboard",
+  created() {
+    this.getMenus()
+  },
   methods: {
+    getMenus() {
+      this.$request.get('http://localhost:8000/api/menus/build').then(res => {
+        this.menuList = res.data
+        console.log(this.menuList);
+      })
+    },
     logout() {
       //请求后端接口删除该用户的token，同时前端Cookies也要删除该用户的token
       this.$request.delete('http://localhost:8000/auth/logout').then(res => {
@@ -132,7 +115,8 @@ export default {
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄'
       }],
-      multipleSelection: []
+      multipleSelection: [],
+      menuList: []
     }
   }
 }

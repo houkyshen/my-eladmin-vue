f74eb683ff0b920932395bc079300948ebae1eb3
HoukyShen <786501105@qq.com>
2021-08-15 23:03:45 +0800

    4.2获取用户列表信息

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,54 +1,79 @@
 <template>
-  <el-table
-      :data="tableData"
-      border
-      style="width: 100%">
-    <el-table-column
-        fixed
-        prop="date"
-        label="日期"
-        width="150">
-    </el-table-column>
-    <el-table-column
-        prop="name"
-        label="姓名"
-        width="120">
-    </el-table-column>
-    <el-table-column
-        prop="province"
-        label="省份"
-        width="120">
-    </el-table-column>
-    <el-table-column
-        prop="city"
-        label="市区"
-        width="120">
-    </el-table-column>
-    <el-table-column
-        prop="address"
-        label="地址"
-        width="300">
-    </el-table-column>
-    <el-table-column
-        prop="zip"
-        label="邮编"
-        width="120">
-    </el-table-column>
-    <el-table-column
-        fixed="right"
-        label="操作"
-        width="100">
-      <template slot-scope="scope">
-        <el-button type="text" size="small">查看</el-button>
-        <el-button type="text" size="small">编辑</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+    <!--用户列表信息表格-->
+    <el-table
+        :data="tableData"
+        style="width: 100%">
+      <el-table-column
+          fixed
+          prop="username"
+          label="用户名"
+          width="150">
+      </el-table-column>
+      <el-table-column
+          prop="nickName"
+          label="昵称"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="gender"
+          label="性别"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="phone"
+          label="电话"
+          width="200">
+      </el-table-column>
+      <el-table-column
+          prop="email"
+          label="邮箱"
+          width="200">
+      </el-table-column>
+      <el-table-column
+          prop="dept.name"
+          label="部门"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="enabled"
+          label="状态"
+          width="120">
+        <template slot-scope="scope">
+          <el-switch ref="enabled"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column
+          :show-overflow-tooltip="true"
+          prop="createTime"
+          label="创建日期"
+          width="200">
+      </el-table-column>
+      <el-table-column
+          prop="operation"
+          label="操作"
+          width="120">
+        <template slot-scope="scope">
+          <el-button @click="handleClick" type="text" size="small">Edit</el-button>
+          <el-button type="text" size="small">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 </template>
 
 <script>
 export default {
   name: "User",
+  created() {
+    this.getUserInfo()
+  },
+  methods:{
+    getUserInfo() {
+      this.$request.get('http://localhost:8000/api/users').then(res => {
+        console.log(res.data);
+        this.tableData = res.data.content
+      })
+    }
+  },
   data() {
     return {
       tableData: [{
